f99d164e30261ebffc3c1dbf331e7b61d201de87
Gyusun Yeom <omniavinco@gmail.com>
2016-03-20 11:37:54 +0900

    move js scripts to root

--- js/application.tsx
+++ js/application.tsx
@@ -1,4 +1,4 @@
-/// <reference path="typings/main.d.ts" />
+/// <reference path="../typings/main.d.ts" />
 /// <reference path="interfaces.d.ts" />
 
 import React = require('react');
--- js/nav.tsx
+++ js/nav.tsx
@@ -1,4 +1,4 @@
-/// <reference path="typings/main.d.ts" />
+/// <reference path="../typings/main.d.ts" />
 
 import React = require('react');
 import _s = require('underscore.string');
